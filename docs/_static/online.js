d0bceaaa69d07e21a15def84fdfab22f7b2bdc90
Tyler Butler <tyler@tylerbutler.com>
2021-09-03 16:16:08 -0700

    Automatic redirect

--- docs/_static/online.js
+++ docs/_static/online.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var url = "https://docs.microsoft.com/en-us/microsoft-365/cloud-storage-partner-program/online/";
+    var url = "https://docs.microsoft.com/en-us/microsoft-365/cloud-storage-partner-program/online/";
 
     var grid = $("div.wy-grid-for-nav");
     if(grid) {
@@ -7,7 +7,12 @@ $(document).ready(function() {
         <div class="admonition warning">
     <p class="admonition-title">Documentation has moved</p>
     <p>This documentation has moved to <a href="${url}">${url}</a></p>
+    <p>This page will automatically redirect to the new URL after three seconds.</a></p>
     </div>
     </div>`);
     }
+
+    window.setTimeout(function () {
+        location.href = url;
+    }, 3000);
 });
